8863914e27df8dde37001413d964e40fcc3ecafd
Caleb Oki <caleboki@gmail.com>
2025-06-19 22:27:46 -0400

    fix: correct case-sensitive import path for AppLayout in PhotoUpload.vue

--- resources/js/pages/Photos/PhotoUpload.vue
+++ resources/js/pages/Photos/PhotoUpload.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppLayout from '@/Layouts/AppLayout.vue'
+import AppLayout from '@/layouts/AppLayout.vue'
 import { Button } from '@/components/ui/button'
 import {
     Card,
